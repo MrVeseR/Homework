d474881e0fdfb1e9deca07173329596a9560d941
MrVeseR <vivalkuzya1107@gmail.com>
2024-09-19 20:24:34 +0300

    Done_Lesson_1

---
+++ Lesson1/Fird.py
@@ -0,0 +1,3 @@
+import math
+L = float(input())
+print( f"T = { round(2*math.pi*((L/9.81)**(1/2)), 2) }")
---
+++ Lesson1/Second.py
@@ -0,0 +1,6 @@
+x, y = 10, 55
+print(f"x = {x}", f"y = {y}")
+z = y
+y = x
+x = z
+print(f"x = {x}", f"y = {y}")

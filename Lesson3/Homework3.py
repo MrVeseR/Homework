25c3414e4d739d90de111f6e7d1c99b0e5e7f3b3
MrVeseR <vivalkuzya1107@gmail.com>
2024-09-28 18:58:08 +0300

    Homework3_done

---
+++ Lesson3/Homework3.py
@@ -0,0 +1,22 @@
+# 1
+# n = int(input("Введите верхний предел \n"))
+# ans = 0
+
+# if n > 100 or n < 1: print("Введите натуральное число, меньшее или равное 100!!!")
+# else:
+#     for i in range(n + 1):
+#         ans += i**3
+#     print(ans)
+
+
+# 2
+# for a in range(1, 10):
+#     for b in range(1, 10):
+#         print(f"{a * b} \t", end="")
+#     print("\n")
+
+
+# 3
+# m = [3, 4, 5, 6, 1]
+# for i in range(len(m) - 1, -1, -1):
+#     print(m[i])
